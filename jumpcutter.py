3aa5e35bf282d37b29fcfd869441f10f157e0ade
mrbesen <y.g.2@gmx.de>
2020-03-26 23:21:15 +0100

    merged 2 ffmpeg commands

--- jumpcutter.py
+++ jumpcutter.py
@@ -109,12 +109,11 @@ AUDIO_FADE_ENVELOPE_SIZE = 400 # smooth out transitiion's audio by quickly fadin
 
 createPath(TEMP_FOLDER)
 
-command = FFMPEGCMD + " -i "+INPUT_FILE+" -qscale:v "+str(FRAME_QUALITY)+" "+TEMP_FOLDER+"/frame%06d.jpg -hide_banner"
+command = FFMPEGCMD + " -i "+INPUT_FILE+" -qscale:v "+str(FRAME_QUALITY)+" "+TEMP_FOLDER+"/frame%06d.jpg -ab 160k -ac 2 -ar "+str(SAMPLE_RATE)+" -vn "+TEMP_FOLDER+"/audio.wav"
 subprocess.call(command, shell=True)
 
-command = FFMPEGCMD + " -i "+INPUT_FILE+" -ab 160k -ac 2 -ar "+str(SAMPLE_RATE)+" -vn "+TEMP_FOLDER+"/audio.wav"
-
-subprocess.call(command, shell=True)
+#command = FFMPEGCMD + " -i "+INPUT_FILE+" -ab 160k -ac 2 -ar "+str(SAMPLE_RATE)+" -vn "+TEMP_FOLDER+"/audio.wav"
+#subprocess.call(command, shell=True)
 
 command = FFMPEGCMD + " -i "+TEMP_FOLDER+"/input.mp4 2>&1"
 f = open(TEMP_FOLDER+"/params.txt", "w")
